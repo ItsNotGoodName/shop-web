5e51782307631bae6ca58d25080ab3c3b493aefe
ItsNotGoodName <gurnaindeol@gmail.com>
2020-09-27 22:30:05 -0700

    Search Icon

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,20 +1,20 @@
-import { Input, Flex, Button } from "@chakra-ui/core";
+import { Input, Flex, Button, IconButton } from "@chakra-ui/core";
 import React from "react";
 
 const SearchBox: React.FC = () => {
   return (
     <Flex mx={1} flexGrow={1}>
       <Input rounded="false" ml={1} w="100%" mt="auto" mb="auto"></Input>
-      <Button
+      <IconButton
+        aria-label="search"
+        icon="search"
         background="#a9e1ef"
         mt="auto"
         mb="auto"
         mr={1}
         rounded="false"
         roundedRight="true"
-      >
-        Search
-      </Button>
+      ></IconButton>
     </Flex>
   );
 };
